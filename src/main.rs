2cfe42dabe6991c60f3b58e7e658444e4e007342
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2024-06-03 01:00:44 +0500

    init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+fn main() {
+    println!("Hello, world!");
+}
