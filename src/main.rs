581f4491e1328b9715348915d07c50aba9b7cfce
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2024-06-03 01:40:41 +0500

    listen actic server

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,15 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
