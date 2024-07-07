14645531cc13627ecf612ee64e2d175080fcb79c
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2024-07-07 23:05:51 +0500

    create and update all migrations

--- migrations/2024-07-07-122623_create_reviews/down.sql
+++ migrations/2024-07-07-122623_create_reviews/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS reviews CASCADE;
--- migrations/2024-07-07-122623_create_reviews/up.sql
+++ migrations/2024-07-07-122623_create_reviews/up.sql
@@ -1,9 +1,9 @@
 -- Your SQL goes here
-CREATE TABLE reviews IF NOT EXISTS {
+CREATE TABLE IF NOT EXISTS reviews (
   id UUID PRIMARY KEY,
   body TEXT NOT NULL,
   rating FLOAT8 NOT NULL,
   item_id UUID NULL REFERENCES items(id),
-  user_id UUID NULL REFERENCES users(id)
+  user_id UUID NULL REFERENCES users(id),
   restaurant_id UUID NULL REFERENCES restaurants(id)
-};
+);
