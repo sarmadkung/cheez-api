d7e461f7474737124eb6ec8d819b761953a1883e
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2024-07-14 18:14:10 +0500

    add new handlers to restataurant

--- src/handlers/restaurant.rs
+++ src/handlers/restaurant.rs
@@ -1,10 +1,16 @@
 use actix_web::{ HttpResponse, Responder, web};
 use diesel::prelude::*;
+use serde::Deserialize;
 use crate::db_conn::establish_connection;
-use crate::models::restaurant::{CreateRestaurant, Restaurant};
+use crate::models::restaurant::{CreateRestaurant, Restaurant,UpdateRestaurant};
 use crate::schema;
 use uuid::Uuid;
 
+
+#[derive(Deserialize)]
+pub struct RestaurantIdRequestPath {
+    id: Uuid,
+}
 
 pub async fn restaurants() -> impl Responder {
     let connection = &mut establish_connection();
@@ -13,6 +19,24 @@ pub async fn restaurants() -> impl Responder {
         .expect("Error loading restaurants");
     println!("{:?}", restaurants);
     HttpResponse::Ok().json(restaurants)
+}
+
+pub async fn my_restaurants() -> impl Responder {
+    let connection = &mut establish_connection();
+    let restaurants = schema::restaurants::table
+        .filter(schema::restaurants::user_id.eq(Uuid::new_v4()))
+        .load::<Restaurant>(connection)
+        .expect("Error loading restaurants");
+    HttpResponse::Ok().json(restaurants)
+}
+
+pub async fn restaurant(path: web::Path<RestaurantIdRequestPath>) -> impl Responder {
+    let connection = &mut establish_connection();
+    let restaurant = schema::restaurants::table
+    .filter(schema::restaurants::id.eq(path.id))
+        .load::<Restaurant>(connection)
+        .expect("Error loading restaurants");
+    HttpResponse::Ok().json(restaurant)
 }
 
 
@@ -33,14 +57,31 @@ pub async fn create(restaurant_data: web::Json<CreateRestaurant>) -> impl Respon
     HttpResponse::Ok().body("New restaurant added")
 }
 
-pub fn update(restaurant_id:Uuid) {
+pub async fn update(path: web::Path<RestaurantIdRequestPath>,restaurant_data:web::Json<UpdateRestaurant>) -> impl Responder {
     let connection = &mut establish_connection();
     let restaurant = schema::restaurants::table
-        .filter(schema::restaurants::id.eq(restaurant_id.clone()))
+        .filter(schema::restaurants::id.eq(path.id.clone()))
         .first::<Restaurant>(connection)
         .expect("Error loading restaurant");
     diesel::update(&restaurant)
-        .set(schema::restaurants::rating.eq(4.0))
+        .set(schema::restaurants::name.eq(restaurant_data.name.clone()))
         .execute(connection)
         .expect("Error updating restaurant");
+    diesel::update(&restaurant)
+        .set(schema::restaurants::location.eq(restaurant_data.location.clone()))
+        .execute(connection)
+        .expect("Error updating restaurant");
+    HttpResponse::Ok().body("Restaurant updated")
+}
+
+pub async fn delete(path: web::Path<RestaurantIdRequestPath>) -> impl Responder {
+    let connection = &mut establish_connection();
+    let restaurant = schema::restaurants::table
+        .filter(schema::restaurants::id.eq(path.id.clone()))
+        .first::<Restaurant>(connection)
+        .expect("Error loading restaurant");
+    diesel::delete(&restaurant)
+        .execute(connection)
+        .expect("Error deleting restaurant");
+    HttpResponse::Ok().body("Restaurant deleted")
 }
