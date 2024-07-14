92c2e800e0cd22f8b746659d9a403d433b8a5638
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2024-07-14 18:33:21 +0500

    move routes to routes folder and fix env loading

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,2 +1,4 @@
 pub mod restaurant;
+pub mod user;
 pub mod auth;
+pub mod menu;
--- src/routes/restaurant.rs
+++ src/routes/restaurant.rs
@@ -1,15 +1,18 @@
 
 /// create restaurant all routes like create, read, update, delete
 
-use actix_web::{web, HttpResponse, Responder};
-use crate::handlers::restaurant;
+use actix_web::web;
+
+use crate::handlers::restaurant::{create, restaurants,update,restaurant,delete,my_restaurants};
 
 pub fn restaurant_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/restaurant")
-            // .route("/get", web::get().to(|| HttpResponse::Ok().json(restaurant::restaurants())))
-            // .route("/create", web::post().to(restaurant::create))
-            // .route("/update", web::put().to(restaurant::update))
-            // .route("/delete", web::delete().to())
+        web::scope("/restaurants")
+        .route("", web::get().to(restaurants))
+        .route("/create", web::post().to(create))
+        .route("/update/{id}", web::put().to(update))
+        .route("/{id}", web::get().to(restaurant))
+        .route("delete/{id}", web::delete().to(delete))
+        .route("/my", web::get().to(my_restaurants)),
     );
 }
